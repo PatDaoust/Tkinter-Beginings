4d73e099a5ec5233587ac987035a471751bf4ebd
Cataluna <catalunalilith2680@gmail.com>
2021-09-16 14:58:42 -0400

    text editior exercise outlined

--- RealPython tutorial code Text Editor exercise.py
+++ RealPython tutorial code Text Editor exercise.py
@@ -5,3 +5,13 @@ Created on Thu Sep 16 12:41:41 2021
 @author: catal
 """
 
+# whole window should have a minimum height of 800 pixels
+
+# in frame1, not auto-resizing:
+# A Button widget called btn_open for opening a file for editing
+# A Button widget called btn_save for saving a file
+
+# in frame2, auto-resizing:
+# A TextBox widget called txt_edit for creating and editing the text file
+# minimum width of 800 pixels
+# .rowconfigure() and .columnconfigure() to 800, weight of 1.
